3f84fe84fd5f1acaeb15f7ceb4437fd55f4db379
Jared Saul <JaronOrchard@gmail.com>
2012-09-20 23:50:24 -0500

    Added Chris's textures; Cells now have floor/ceiling textures instead of colors

--- src/solais/Cell.java
+++ src/solais/Cell.java
@@ -6,8 +6,8 @@ public class Cell {
 	private int eastWallTexture;
 	private int southWallTexture;
 	private int westWallTexture;
-	private Color floorColor;
-	private Color ceilingColor;
+	private int floorTexture;
+	private int ceilingTexture;
 	private boolean visible;
 	private boolean solid;
 	private boolean breakable;
@@ -16,17 +16,19 @@ public class Cell {
 	public int getEastWallTexture() { return eastWallTexture; }
 	public int getSouthWallTexture() { return southWallTexture; }
 	public int getWestWallTexture() { return westWallTexture; }
+	public int getFloorTexture() { return floorTexture; }
+	public int getCeilingTexture() { return ceilingTexture; }
 	public boolean getVisible() { return visible; }
 	public boolean getSolid() { return solid; }
 	public boolean getBreakable() { return breakable; }
 	
-	public Cell(int nTexture, int eTexture, int sTexture, int wTexture, Color floorColor, Color ceilingColor, boolean visible, boolean solid, boolean breakable) {
+	public Cell(int nTexture, int eTexture, int sTexture, int wTexture, int fTexture, int cTexture, boolean visible, boolean solid, boolean breakable) {
 		this.northWallTexture = nTexture;
 		this.eastWallTexture = eTexture;
 		this.southWallTexture = sTexture;
 		this.westWallTexture = wTexture;
-		this.floorColor = floorColor;
-		this.ceilingColor = ceilingColor;
+		this.floorTexture = fTexture;
+		this.ceilingTexture = cTexture;
 		this.visible = visible;
 		this.solid = solid;
 		this.breakable = breakable;
@@ -37,19 +39,18 @@ public class Cell {
 		glPushMatrix();
 			glTranslatef((float)x, 0f, (float)z); // Move cell to where it is supposed to be
 			
-			glDisable(GL_TEXTURE_2D);
+			glEnable(GL_TEXTURE_2D);
 			if (!hasNorthWall() || !hasEastWall() || !hasSouthWall() || !hasWestWall()) {
 				drawFloor();
 				drawCeiling();
 			}	
 			if (this.visible) {
-				glEnable(GL_TEXTURE_2D);
 				if (hasNorthWall()) { drawNorthWall(); }
 				if (hasEastWall()) { drawEastWall(); }
 				if (hasSouthWall()) { drawSouthWall(); }
 				if (hasWestWall()) { drawWestWall(); }
-				glDisable(GL_TEXTURE_2D);
 			}
+			glDisable(GL_TEXTURE_2D);
 			
 		glPopMatrix();
 	}
@@ -60,24 +61,36 @@ public class Cell {
 	private boolean hasWestWall() { return westWallTexture != 0; }
 	
 	private void drawFloor() {
-		glColor3f(floorColor.getRed(), floorColor.getGreen(), floorColor.getBlue());
+		glColor3f(1f, 1f, 1f);
 		glPushMatrix();
+			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+			glBindTexture(GL_TEXTURE_2D, this.floorTexture);
 			glBegin(GL_QUADS);
-				glVertex3f(0f, 0f, 0f);
-				glVertex3f(0f, 0f, 1f);
+				glTexCoord2d(0,0); 
 				glVertex3f(1f, 0f, 1f);
+				glTexCoord2d(1,0);
+				glVertex3f(0f, 0f, 1f);
+				glTexCoord2d(1,1); 
+				glVertex3f(0f, 0f, 0f);
+				glTexCoord2d(0,1); 
 				glVertex3f(1f, 0f, 0f);
 			glEnd();
 		glPopMatrix();
 	}
 	
 	private void drawCeiling() {
-		glColor3f(ceilingColor.getRed(), ceilingColor.getGreen(), ceilingColor.getBlue());
+		glColor3f(1f, 1f, 1f);
 		glPushMatrix();
+			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+			glBindTexture(GL_TEXTURE_2D, this.ceilingTexture);
 			glBegin(GL_QUADS);
-				glVertex3f(0f, 1f, 0f);
-				glVertex3f(0f, 1f, 1f);
+				glTexCoord2d(0,0); 
 				glVertex3f(1f, 1f, 1f);
+				glTexCoord2d(1,0);
+				glVertex3f(0f, 1f, 1f);
+				glTexCoord2d(1,1); 
+				glVertex3f(0f, 1f, 0f);
+				glTexCoord2d(0,1); 
 				glVertex3f(1f, 1f, 0f);
 			glEnd();
 		glPopMatrix();
--- src/solais/Textures.java
+++ src/solais/Textures.java
@@ -1,22 +1,46 @@
 package solais;
-import java.io.InputStream;
 
 public class Textures {
+	
+	public static final int NONE = 0;
+	
 	public static final int HALLWAYTEST = 1;
 	public static final int HALLWAYTEST_GRATE = 2;
 	public static final int STONE_WALL = 3;
 	public static final int UNDER_CONSTRUCTION = 99;
 	
-	public final static InputStream HALLWAYTEST_RESOURCE = ClassLoader.getSystemResourceAsStream("resources/textures/HallwayTest.png");
-	public final static InputStream HALLWAYTEST_GRATE_RESOURCE = ClassLoader.getSystemResourceAsStream("resources/textures/HallwayTestGrate.png");
-	public final static InputStream STONE_WALL_RESOURCE = ClassLoader.getSystemResourceAsStream("resources/textures/StoneWall.png");	
-	public final static InputStream UNDER_CONSTRUCTION_RESOURCE = ClassLoader.getSystemResourceAsStream("resources/textures/UnderConstruction.png");	
+	// 1000s - Walls
+	public static final int BLUE_BRICK = 1001;
+	public static final int ORANGE_BRICK = 1002;
+	public static final int RED_BRICK = 1003;
+	
+	// 2000s - Floors
+	public static final int BLUE_TILE = 2001;
+	public static final int ORANGE_TILE = 2002;
+	public static final int RED_TILE = 2003;
+	public static final int GENERIC_FLOOR = 2998;
+	public static final int GENERIC_ALT_FLOOR = 2999;
+	
+	// 3000s - Ceilings
+	public static final int GENERIC_CEILING = 3999;
 	
 	public static void initializeTextures() {
-		TextureLoader.loadTexture(HALLWAYTEST, TextureLoader.loadImage(HALLWAYTEST_RESOURCE));
-		TextureLoader.loadTexture(HALLWAYTEST_GRATE, TextureLoader.loadImage(HALLWAYTEST_GRATE_RESOURCE));
-		TextureLoader.loadTexture(STONE_WALL, TextureLoader.loadImage(STONE_WALL_RESOURCE));
-		TextureLoader.loadTexture(UNDER_CONSTRUCTION, TextureLoader.loadImage(UNDER_CONSTRUCTION_RESOURCE));
+		TextureLoader.loadTexture(HALLWAYTEST, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/HallwayTest.png")));
+		TextureLoader.loadTexture(HALLWAYTEST_GRATE, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/HallwayTestGrate.png")));
+		TextureLoader.loadTexture(STONE_WALL, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/StoneWall.png")));
+		TextureLoader.loadTexture(UNDER_CONSTRUCTION, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/UnderConstruction.png")));
+		
+		TextureLoader.loadTexture(BLUE_BRICK, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/blue_brick.png")));
+		TextureLoader.loadTexture(ORANGE_BRICK, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/orange_brick.png")));
+		TextureLoader.loadTexture(RED_BRICK, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/red_brick.png")));
+		
+		TextureLoader.loadTexture(BLUE_TILE, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/blue_tile.png")));
+		TextureLoader.loadTexture(ORANGE_TILE, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/orange_tile.png")));
+		TextureLoader.loadTexture(RED_TILE, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/red_tile.png")));
+		TextureLoader.loadTexture(GENERIC_FLOOR, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/FloorColor.png")));
+		TextureLoader.loadTexture(GENERIC_ALT_FLOOR, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/AltFloorColor.png")));
+		
+		TextureLoader.loadTexture(GENERIC_CEILING, TextureLoader.loadImage(ClassLoader.getSystemResourceAsStream("resources/textures/CeilingColor.png")));
 		
 	}
 	
